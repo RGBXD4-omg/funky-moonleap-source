b0ffb762820f97f2bb65f59db6eac88920ba299c
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-07-29 09:46:19 +0800

    Update MusicBeat.hx

--- source/meta/MusicBeat.hx
+++ source/meta/MusicBeat.hx
@@ -46,7 +46,7 @@ class MusicBeatState extends FNFUIState
 	
 	#if android
 	public function addVirtualPad(?DPad:FlxDPadMode, ?Action:FlxActionMode) {
-		_virtualpad = new FlxVirtualPad(DPad, Action, 0.75, ClientPrefs.globalAntialiasing);
+		_virtualpad = new FlxVirtualPad(DPad, Action, 0.75);
 		add(_virtualpad);
 		controls.setVirtualPadUI(_virtualpad, DPad, Action);
 		trackedinputsUI = controls.trackedinputsUI;
@@ -241,7 +241,7 @@ class MusicBeatSubState extends FlxSubState
 	
 	#if android
 	public function addVirtualPad(?DPad:FlxDPadMode, ?Action:FlxActionMode) {
-		_virtualpad = new FlxVirtualPad(DPad, Action, 0.75, ClientPrefs.globalAntialiasing);
+		_virtualpad = new FlxVirtualPad(DPad, Action, 0.75);
 		add(_virtualpad);
 		controls.setVirtualPadUI(_virtualpad, DPad, Action);
 		trackedinputsUI = controls.trackedinputsUI;

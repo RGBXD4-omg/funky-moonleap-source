9d51403489bcb0c26a7ce3bb5a02bcc0386f2805
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-08-01 11:06:50 +0800

    Update FlxNewHitbox.hx

--- source/android/FlxNewHitbox.hx
+++ source/android/FlxNewHitbox.hx
@@ -52,13 +52,13 @@ class FlxNewHitbox extends FlxSpriteGroup
 		}
 
 		}
-		else
-			{
+		else if (SaveData.trueSettings.get('Way Of Space'))
+		{
 		add(buttonLeft = createHint(0, 0, Std.int(FlxG.width / 4), FlxG.height, 0xFF00FF));
 		add(buttonDown = createHint(FlxG.width / 4, 0, Std.int(FlxG.width / 4), FlxG.height, 0x00FFFF));
 		add(buttonUp = createHint(FlxG.width / 2, 0, Std.int(FlxG.width / 4), FlxG.height, 0x00FF00));
 		add(buttonRight = createHint((FlxG.width / 2) + (FlxG.width / 4), 0, Std.int(FlxG.width / 4), FlxG.height, 0xFF0000));
-			}
+		}
 		
 		scrollFactor.set();
 	}
